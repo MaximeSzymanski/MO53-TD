abf977959efefb386c0cbd9ffd3cc89bf55e8b4b
Lucas Bruton <lucas.bruton9@gmail.com>
2023-04-17 21:04:46 +0200

    added bin_writer

--- refactored_code/main.cpp
+++ refactored_code/main.cpp
@@ -7,6 +7,7 @@
 #include "writer.h"
 #include "screen_writer.h"
 #include "text_writer.h"
+#include "bin_writer.h"
 
 using namespace std;
 
@@ -17,7 +18,7 @@ using namespace std;
 int main() {
     shared_ptr<loader> l(new bin_loader());
     shared_ptr<storage_type> s(new vector());
-    shared_ptr<writer> w = make_shared<text_writer>(text_writer("destination.txt"));
+    shared_ptr<writer> w = make_shared<bin_writer>(bin_writer("destination.bin"));
     storage_type& ref = l->load(*s, "binary.bin");
     w->write(ref);
 
--- refactored_code/text_writer.cpp
+++ refactored_code/text_writer.cpp
@@ -10,13 +10,15 @@ text_writer& text_writer::operator=(const text_writer& p_text_writer) {
     this->destination = p_text_writer.destination;
     return *this;
 }
+
 void text_writer::write(storage_type& storage) {
-    ofstream f{destination};
+    ofstream f{destination, std::ios::binary};
     if(!f.is_open()) {
         cout << "File cannot be opened for writing." << endl;
     }
 
     for (size_t i=0; i<storage.size(); ++i) {
-        f << storage.get_element(i) << endl;
+        const double &value = storage.get_element(i);
+        f.write(reinterpret_cast<const char *>(&value), sizeof(double));
     }
 }
